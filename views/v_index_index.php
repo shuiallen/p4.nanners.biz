0b23ffd9570cc7d52c0d688ac93ba0f314622154
Shirley Hui <shuiallen@gmail.com>
2013-12-19 13:13:41 -0500

    fix bugs

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -11,11 +11,15 @@ Welcome to <strong>What Am I Doing Today?</strong>
 <p>
 
 This website will help you keep track of what you need to do today
-<li>create tasks</li>
-<li>mark a task as completed</li>
-<li>see a list of incomplete tasks</li>
-<li>track time spent on a task</li>
-<li>see tasks that have recorded time</li>
+<ul>
+	<li>create tasks</li>
+	<li>mark a task as completed</li>
+	<li>see a list of incomplete tasks</li>
+	<li>track time spent on a task</li>
+	<li>see tasks that have recorded time</li>
+</ul>
 
-<p>Some things that are implemented but incomplete</p>
-<li>make a quick list</li>
+<p>Partially implemented</p>
+<ul>
+	<li>make a quick list</li>
+</ul>
